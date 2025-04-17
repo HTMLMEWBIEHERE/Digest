65f4687a5b2ef6e7080e892886f497c1675ce608
criscelann <delosreyescriscelann@gmail.com>
2025-04-17 20:50:18 +0800

    announcement_page

--- superadmin_content/sa_add_carousel.php
+++ superadmin_content/sa_add_carousel.php
@@ -133,8 +133,6 @@ $carousel_images = $select_carousel_images->fetchAll(PDO::FETCH_ASSOC);
 <!-- Add Carousel Image Section -->
 <section class="add-carousel">
     <h1 class="heading">Add Carousel Image</h1>
-    <h2>Note: The image size should be 4000 x 1500</h2>
-
     <?php if (!empty($message)): ?>
         <?php foreach ($message as $msg): ?>
             <div class="message">
